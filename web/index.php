cd1a83c46fdaf10d59014c35cea69576a73065a1
fatimarafiqui <fatima.rafiqui@gmail.com>
2019-01-14 06:17:36 +0530

    fix

--- web/index.php
+++ web/index.php
@@ -1,56 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width"/>
-		<meta name="author" content="Fatima Rafiqui"/>
-		<meta name="description" content="Fatima Rafiqui, Former Student Developer @ GSoC, DevOps intern @ Oracle Cloud Infrastructure. A thinker, a technophile & an engineer. She loves travelling, programming, designs & volunteering." />
-		<link rel="icon" href="/favicon.ico"/>
-        <title>Resume | Fatima Rafiqui</title>
-		<meta property="og:type" content="profile" />
-		<meta property="og:title" content="Resume | Fatima Rafiqui" />
-		<meta property="og:description" content="Resume of Fatima Rafiqui" />
-		<meta property="og:url" content="http://fatimarafiqui.com/resume/" />
-		<meta property="og:image" content="http://fatimarafiqui.com/resume/img/og_terminal_resume.png" />
-		<meta property="og:site_name" content="The Extra Mile" />
-		<meta name="twitter:card" content="summary" />
-		<meta name="twitter:site" content="@fatima_rafiqui" />
-		<meta name="twitter:site:id" content="fatima_rafiqui" />
-		<link href="/css/style.css" rel="stylesheet"/>
-		<link rel="stylesheet" href="/fonts/css/font-awesome.min.css"/>
-		<link href="/css/jquery.terminal-0.11.5.css" rel="stylesheet"/>
-	</head>
-	<body>
-		<canvas id="bodyCanvas"></canvas>
-		<div id="content">
-			<div class="icons">
-				<span class="icons-item">
-					<a href="/" target="_blank">
-						<i class="fa fa-home" aria-hidden="true"></i>
-					</a>
-				</span>
-				<span class="icons-item">
-					<a href="https://fatimarafiqui.com/download-resume" target="_blank">
-						<i class="fa fa-file-pdf-o" aria-hidden="true"></i>
-					</a>
-				</span>
-			</div>
-			<div class="shell-wrap" style="display:none;">
-				<p class="shell-top-bar">
-					<a href="#" class="close left">close</a>
-					<a href="#" class="minimize left">minimize</a>
-					<a href="#" class="maximize left">maximize</a>
-					<span class="title"></span>
-				</p>
-				<div class="shell-body span12" id="shell-body"></div>
-			</div>
-		</div>
-		<script src="/js/jquery-3.1.0.min.js"></script>
-		<script src="/js/jquery.terminal-0.11.11.min.js"></script>
-		<script src="/js/jquery.mousewheel-min.js"></script>
-		<script src="/js/liveBg.js"></script>
-		<script src="/js/resume.js"></script>
-	</body>
+  <head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width"/>
+    <meta name="author" content="Fatima Rafiqui"/>
+    <meta name="description" content="Fatima Rafiqui, Former Student Developer @ GSoC, DevOps intern @ Oracle Cloud Infrastructure. A thinker, a technophile & an engineer. She loves travelling, programming, designs & volunteering." />
+    <link rel="icon" href="/favicon.ico"/>
+    <title>Resume | Fatima Rafiqui</title>
+    <meta property="og:type" content="profile" />
+    <meta property="og:title" content="Resume | Fatima Rafiqui" />
+    <meta property="og:description" content="Resume of Fatima Rafiqui" />
+    <meta property="og:url" content="http://fatimarafiqui.com/resume/" />
+    <meta property="og:image" content="http://fatimarafiqui.com/resume/img/og_terminal_resume.png" />
+    <meta property="og:site_name" content="The Extra Mile" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@fatima_rafiqui" />
+    <meta name="twitter:site:id" content="fatima_rafiqui" />
+    <link href="/css/style.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="/fonts/css/font-awesome.min.css"/>
+    <link href="/css/jquery.terminal-0.11.5.css" rel="stylesheet"/>
+  </head>
+  <body>
+    <canvas id="bodyCanvas"></canvas>
+    <div id="content">
+      <div class="icons">
+        <span class="icons-item">
+          <a href="/" target="_blank">
+            <i class="fa fa-home" aria-hidden="true"></i>
+          </a>
+        </span>
+        <span class="icons-item">
+          <a href="https://fatimarafiqui.com/download-resume" target="_blank">
+            <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+          </a>
+        </span>
+      </div>
+      <div class="shell-wrap" style="display:none;">
+        <p class="shell-top-bar">
+          <a href="#" class="close left">close</a>
+          <a href="#" class="minimize left">minimize</a>
+          <a href="#" class="maximize left">maximize</a>
+          <span class="title"></span>
+        </p>
+        <div class="shell-body span12" id="shell-body"></div>
+      </div>
+    </div>
+    <script src="/js/jquery-3.1.0.min.js"></script>
+    <script src="/js/jquery.terminal-0.11.11.min.js"></script>
+    <script src="/js/jquery.mousewheel-min.js"></script>
+    <script src="/js/liveBg.js"></script>
+    <script src="/js/resume.js"></script>
+  </body>
 </html>
 
--- web/js/resume.js
+++ web/js/resume.js
@@ -3,171 +3,164 @@ $(document).ready(function(){
 	if($(document).width() > 740) {
 		$(".icons").show();
 	}
-	$.getJSON('https://freegeoip.net/json/?callback=?',function(data) {
-		$(function() {
-			var ip =  '127.0.0.1';
-			if(typeof data.ip !== 'undefined') {
-				ip = data.ip;
-			}
-			var domain = window.location.hostname;
-			var terminal_prompt = domain + ":~ root$";
-			var title = "webserver@" + domain + " ~/root";
-			localTime = new Date();
-			timestamp = localTime.toLocaleString();
-			var w = $('.shell-wrap').width() - 20;
-			var h = $(document).height() < 400 ? 300 : 400;
-			var anim = false;
-			var titleText = $(".shell-top-bar .title");
-			titleText.text(title);
-			
-			function typed(finish_typing) {
-				return function(term, message, delay, finish) {
-					anim = true;
-					var prompt = term.get_prompt();
-					var c = 0;
-					if (message.length > 0) {
-						term.set_prompt('');
-						var interval = setInterval(function() {
-							term.insert(message[c++]);
-							if (c == message.length) {
-								clearInterval(interval);
-								// execute in next interval
-								setTimeout(function() {
-									// swap command with prompt
-									finish_typing(term, message, prompt);
-									anim = false
-									finish && finish();
-								}, delay);
-							}
-						}, delay);
-					}
-				};
-			}
-			var typed_prompt = typed(function(term, message, prompt) {
-				// swap command with prompt
-				term.set_command('');
-				term.set_prompt(message + ' ');
-			});
-			var typed_message = typed(function(term, message, prompt) {
-				term.set_command('');
-				term.echo(message, {raw:true});
-				term.set_prompt(prompt);
-			});
-			function progress(percent, width) {
-			    var size = Math.round(width*percent/100);
-			    var left = '', taken = '', i;
-			    for (i=size; i--;) {
-			        taken += '=';
-			    }
-			    if (taken.length > 0) {
-			        taken = taken.replace(/=$/, '>');
-			    }
-			    for (i=width-size; i--;) {
-			        left += ' ';
-			    }
-			    return '[' + taken + left + '] ' + percent + '%';
-			}
-			
-			function updateTitle(term, response) {
-				titleText.text(title + "/" + response.title);
-				if (response.title != '') {
-					terminal_prompt = domain + ":~ " + response.title + "$";
-				} else {
-					terminal_prompt = domain + ":~ root$";
-				}
-				term.set_prompt(terminal_prompt + ' ');
-				if(response.msg == '') {
-					return;
-				}
-				if(response.status) {
-					term.echo('<span class="white">' + response.msg + '</span>', {raw:true});
-				} else {
-					term.echo('<span class="red">' + response.msg + '</span>', {raw:true});
-				}
-			};
-			
-			var animation = false;
-			var timer;
-			var prompt;
-			var string;
-			
-			$('#shell-body').terminal(function(cmd, term) {
-				cmd = cmd.trim();
-				if (cmd.length == 0) {
-					return false;
-				}
-				var finish = false;
-				//term.set_prompt(terminal_prompt);
-				var i = 0, size = Math.floor(w/11);
-				prompt = term.get_prompt();
-				string = progress(0, size);
-				term.set_prompt(progress);
-				animation = true;
-				term.focus();
-				var args = {command: cmd};
-				args = $(this).serialize() + "&" + $.param(args);
-				if(cmd != 'clear') {
-					term.set_prompt(string);
-					$.ajax({
-						type: "POST",
-						dataType: "json",
-						url: "/commands.php",
-						data: args,
-						success: function(result) {
-							if (result.animate) {
-								(function loop() {
-								    string = progress(i++, size);
-								    term.set_prompt(string);
-								    if (i < 100) {
-								        timer = setTimeout(loop, 10);
-								    } else {
-								    	if(result.status) {
-								        	term.echo(progress(i, size) + ' [[b;green;]ok]').set_prompt(prompt);
-								        } else {
-								        	term.echo(progress(i, size) + ' [[b;red;]ERROR]').set_prompt(prompt);
-								     	}
-								        animation = false;
-									updateTitle(term, result);
-								    }
-								})();
-							} else {
-								animation = false;
-								updateTitle(term, result);
-							}
-						}
-					});
-				}
-			}, {
-				name: 'xxx',
-				greetings: null,
-				width: w,
-				height: h,
-				onInit: function(term) {
-					var msg_line1 = "Linux " + domain + " 3.2.0-4-amd64 #1 SMP Debian 3.2.60-1+deb7u3 x86_64";
-					var msg_line2 = "Last login: " + timestamp + " from " + ip;
-					typed_message(term, msg_line1, 50, function() {
-						typed_message(term, msg_line2, 50, function() {
-							typed_prompt(term, terminal_prompt, 50);
-						});
-					});
-				},
-				keydown: function(e) {
-					if (anim) {
-						return false;
-					}
-					if (animation) {
-					    if (e.which == 68 && e.ctrlKey) { // CTRL+D
-					        clearTimeout(timer);
-					        animation = false;
-					        term.echo(string + ' [[b;red;]FAIL]')
-					        .set_prompt(prompt);
-					    }
-					    return false;
-					}
-				}
-			});
-		});
-	});
+  $(function() {
+    var ip =  '127.0.0.1';
+    var domain = window.location.hostname;
+    var terminal_prompt = domain + ":~ root$";
+    var title = "webserver@" + domain + " ~/root";
+    localTime = new Date();
+    timestamp = localTime.toLocaleString();
+    var w = $('.shell-wrap').width() - 20;
+    var h = $(document).height() < 400 ? 300 : 400;
+    var anim = false;
+    var titleText = $(".shell-top-bar .title");
+    titleText.text(title);
+    
+    function typed(finish_typing) {
+      return function(term, message, delay, finish) {
+        anim = true;
+        var prompt = term.get_prompt();
+        var c = 0;
+        if (message.length > 0) {
+          term.set_prompt('');
+          var interval = setInterval(function() {
+            term.insert(message[c++]);
+            if (c == message.length) {
+              clearInterval(interval);
+              // execute in next interval
+              setTimeout(function() {
+                // swap command with prompt
+                finish_typing(term, message, prompt);
+                anim = false
+                finish && finish();
+              }, delay);
+            }
+          }, delay);
+        }
+      };
+    }
+    var typed_prompt = typed(function(term, message, prompt) {
+      // swap command with prompt
+      term.set_command('');
+      term.set_prompt(message + ' ');
+    });
+    var typed_message = typed(function(term, message, prompt) {
+      term.set_command('');
+      term.echo(message, {raw:true});
+      term.set_prompt(prompt);
+    });
+    function progress(percent, width) {
+        var size = Math.round(width*percent/100);
+        var left = '', taken = '', i;
+        for (i=size; i--;) {
+            taken += '=';
+        }
+        if (taken.length > 0) {
+            taken = taken.replace(/=$/, '>');
+        }
+        for (i=width-size; i--;) {
+            left += ' ';
+        }
+        return '[' + taken + left + '] ' + percent + '%';
+    }
+    
+    function updateTitle(term, response) {
+      titleText.text(title + "/" + response.title);
+      if (response.title != '') {
+        terminal_prompt = domain + ":~ " + response.title + "$";
+      } else {
+        terminal_prompt = domain + ":~ root$";
+      }
+      term.set_prompt(terminal_prompt + ' ');
+      if(response.msg == '') {
+        return;
+      }
+      if(response.status) {
+        term.echo('<span class="white">' + response.msg + '</span>', {raw:true});
+      } else {
+        term.echo('<span class="red">' + response.msg + '</span>', {raw:true});
+      }
+    };
+    
+    var animation = false;
+    var timer;
+    var prompt;
+    var string;
+    
+    $('#shell-body').terminal(function(cmd, term) {
+      cmd = cmd.trim();
+      if (cmd.length == 0) {
+        return false;
+      }
+      var finish = false;
+      //term.set_prompt(terminal_prompt);
+      var i = 0, size = Math.floor(w/11);
+      prompt = term.get_prompt();
+      string = progress(0, size);
+      term.set_prompt(progress);
+      animation = true;
+      term.focus();
+      var args = {command: cmd};
+      args = $(this).serialize() + "&" + $.param(args);
+      if(cmd != 'clear') {
+        term.set_prompt(string);
+        $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: "/commands.php",
+          data: args,
+          success: function(result) {
+            if (result.animate) {
+              (function loop() {
+                  string = progress(i++, size);
+                  term.set_prompt(string);
+                  if (i < 100) {
+                      timer = setTimeout(loop, 10);
+                  } else {
+                    if(result.status) {
+                        term.echo(progress(i, size) + ' [[b;green;]ok]').set_prompt(prompt);
+                      } else {
+                        term.echo(progress(i, size) + ' [[b;red;]ERROR]').set_prompt(prompt);
+                    }
+                      animation = false;
+                updateTitle(term, result);
+                  }
+              })();
+            } else {
+              animation = false;
+              updateTitle(term, result);
+            }
+          }
+        });
+      }
+    }, {
+      name: 'xxx',
+      greetings: null,
+      width: w,
+      height: h,
+      onInit: function(term) {
+        var msg_line1 = "Linux " + domain + " 3.2.0-4-amd64 #1 SMP Debian 3.2.60-1+deb7u3 x86_64";
+        var msg_line2 = "Last login: " + timestamp + " from " + ip;
+        typed_message(term, msg_line1, 50, function() {
+          typed_message(term, msg_line2, 50, function() {
+            typed_prompt(term, terminal_prompt, 50);
+          });
+        });
+      },
+      keydown: function(e) {
+        if (anim) {
+          return false;
+        }
+        if (animation) {
+            if (e.which == 68 && e.ctrlKey) { // CTRL+D
+                clearTimeout(timer);
+                animation = false;
+                term.echo(string + ' [[b;red;]FAIL]')
+                .set_prompt(prompt);
+            }
+            return false;
+        }
+      }
+    });
+  });
 });
-                            
-                            
